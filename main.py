faa7aa1ee4fc89873f6aca5d4dbf03eac9f92c03
S B <barat.shivansi@gmail.com>
2020-08-06 20:48:14 +0000

    more functions

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-#functions are intructions to do a certain action
+# #functions are intructions to do a certain action
 
-#2 things required for functions: we define them, we call them to work
+# #2 things required for functions: we define them, we call them to work
 
-#!)define function
-def greeting():
-  print("hello")
+# #!)define function
+# def greeting():
+#   print("hello")
 
-#2) call the function to work
-greeting()
+# #2) call the function to work
+# greeting()
 
-def goodbye(name):
-  print("byeee, "+name)
+# def goodbye(name):
+#   print("byeee, "+name)
 
-goodbye("mario")
+# goodbye("mario")
 
-def add(num):
-  for i in range(5):
-    sum=num+1
-    print(sum)
+# def add(num):
+#   for i in range(5):
+#     sum=num+1
+#     print(sum)
 
-    if sum<5:
-      print("less than 5")
-    else:
-      print("greater than 5")
+#     if sum<5:
+#       print("less than 5")
+#     else:
+#       print("greater than 5")
 
-add(3)
+# add(3)
+
+def sandwich():
+  print("salami")
+sandwich()
+
+#create  a function called book that prints out your favorite fantasy book to the console.
+def book(title):
+  print(title)
+
+book("holes")
